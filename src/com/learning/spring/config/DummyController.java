082d3ae1637f49e34822ef892185742c5a8b9893
dddodiya2 <dddodiya2@gmail.com>
2022-05-17 19:14:57 +0530

    Java config mvc application day1

--- src/com/learning/spring/config/DummyController.java
+++ src/com/learning/spring/config/DummyController.java
@@ -1,4 +1,4 @@
-package com.spring.learning;
+package com.learning.spring.config;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
---
+++ src/com/learning/spring/config/MyWebApplicationConfig.java
@@ -0,0 +1,23 @@
+package com.learning.spring.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@ComponentScan("com.learning.spring")
+@EnableWebMvc
+public class MyWebApplicationConfig {
+
+	@Bean
+	public ViewResolver getViewResolver() {
+		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+		viewResolver.setSuffix(".jsp");
+		viewResolver.setPrefix("/WEB-INF/view/");
+		return viewResolver;
+	}
+
+}
